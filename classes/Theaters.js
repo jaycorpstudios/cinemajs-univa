4bac1a9cc384f149b8af7123e6d2204902b6867e
Poble04 <medinagustavo45@gmail.com>
2019-01-16 21:45:39 -0600

    UNIVA-GIT-003 cambios en classes/Theaters y data/theaters

--- classes/Theaters.js
+++ classes/Theaters.js
@@ -1,16 +1,6 @@
-//TODO: UNIVA-GIT-003: theatersData should require data from ../data/theaters Hint: Take a look at Movies.js file.
 
-const theatersData = [
-  {
-    name: 'Cinemex Punto Sao Paulo',
-    id: 4,
-    brand: 'Cinemex',
-    location: { lat: 20.702281, long: -103.376779 },
-    street: 'Valparaiso 2367',
-    state: 'Jalisco',
-    hasVIP: true
-  }
-];
+const theatersData = require('../data/theaters.js');
+
 
 module.exports = class Theaters{
 
